b9d570d699cbbfdcd5d933453df735328d26204a
bloo327 <bliu32799@gmail.com>
2020-06-30 00:12:19 -0700

    added video background, mobile responsiveness next

--- js/app.js
+++ js/app.js
@@ -13,29 +13,10 @@ function animateOnScroll(target, triggerPosition, activeClass, reversible = fals
 }       
 
 window.addEventListener('scroll', ()=>{
-  animateOnScroll('.aboutme', 0.8, 'appear', true)
-  animateOnScroll('.willportrait', .6, 'appear', true)
-  animateOnScroll('.banners', 0.9, 'appear', true)
-  animateOnScroll('.section4', 0.9, 'appear', true)
+  animateOnScroll('.sectiontitle', .7, 'appear', true)
+  animateOnScroll('.aboutme', .8, 'appear', true)
+  animateOnScroll('.willportrait', .7, 'appear', true)
+  animateOnScroll('.col-md-8', .8, 'appear', true)
+  animateOnScroll('.igbtn', .6, 'appear', true)
 })
 
-//
-var body = document.querySelector('body');
-
-body.onmousemove = function(e) {
-  body.style.setProperty("--cx", e.clientX + "px");
-  body.style.setProperty("--cy", e.clientY + "px");
-}
-
-//
-function mobileOpacity(mobileWindow) {
-  if (mobileWindow.matches) { // If media query matches
-    document.body.style.opacity = 1;
-  }
-}
-
-var mobileWindow = window.matchMedia("(max-width: 700px)")
-mobileOpacity(mobileWindow) // Call listener function at run time
-mobileWindow.addListener(mobileOpacity) // Attach listener function on state changes
-
-//
